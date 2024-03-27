8e8a60b1f4ed8748993f0ad5f9494e084e399e6e
ZachMuchamore <146819131+ZachMuchamore@users.noreply.github.com>
2024-03-27 14:31:16 +0000

    game

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    float velocity = 2;
     Rigidbody2D rb;
+    float speed = 2;
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +16,16 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("A"))
+
+        if (Input.GetKey("a") == true)
         {
-            rb.AddForce(rb.velocity);
+            transform.localScale = new Vector3(-3, 3, 3);
+            transform.position = new Vector2(transform.position.x - (speed * Time.deltaTime), transform.position.y);
+        }
+        if (Input.GetKey("d") == true)
+        {
+            transform.localScale = new Vector3(3, 3, 3);
+            transform.position = new Vector2(transform.position.x + (speed * Time.deltaTime), transform.position.y);
         }
     }
 }
